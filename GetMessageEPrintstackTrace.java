e4b08ad2385d065b7f1bbfa56a5701f34a147899
Miguel Soares <162351041+MgsLv@users.noreply.github.com>
2024-08-28 14:45:10 -0300

    Update GetMessageEPrintstackTrace.java

--- GetMessageEPrintstackTrace.java
+++ GetMessageEPrintstackTrace.java
@@ -3,12 +3,23 @@ import java.util.concurrent.ExecutionException;
 
 public class GetMessageEPrintstackTrace {
     public static void main(String args[]) {
+        // Caixa de execução
+
         int x = Integer.parseInt(JOptionPane.showInputDialog("Forneça um número"));
+        /* Variável x recebe valores do tipo String transportados para int pelo metodo parseInt
+         Exibe na tela mensagem de dialogo com o usuário
+         */
+
+
         try {
             int y = x / 0;
+            // Variável y, do tipo int, recebe o valor de x dividido por 0
+
         } catch (Exception erro) {
             JOptionPane.showMessageDialog(null, erro.getMessage());
             erro.printStackTrace();
+
+            // Caso ocorra algum erro, exibe na tela a mensagem de erro getMessage
         }
     }
 }
